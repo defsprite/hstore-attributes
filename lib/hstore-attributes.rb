31c7f32eb0bcd79b1c2f4cb321109e587c22118d
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2012-05-05 02:14:46 +0200

    moving require to top

--- lib/hstore-attributes.rb
+++ lib/hstore-attributes.rb
@@ -5,6 +5,7 @@ class HstoreAttributes < Rails::Railtie
   initializer 'hstore-attributes' do
     ActiveSupport.on_load :active_record do
       Rails.logger.info "loading hstore meta columns"
+      require "hstore-attributes/hstore_columns"
       require "hstore-attributes/activerecord"
     end
   end
--- lib/hstore-attributes/activerecord.rb
+++ lib/hstore-attributes/activerecord.rb
@@ -1,6 +1,4 @@
 # Extends AR to add meta-columns for easy Hstore usage.
-require 'hstore_columns'
-
 module ActiveRecord
 
   module ConnectionAdapters
