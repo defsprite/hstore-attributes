94de9305a975e4bfa25261186c03c64b224aba8f
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2013-03-04 11:57:20 +0000

    remove column instances, move type_cast_code into our domain.

--- lib/hstore-attributes/hstore_columns.rb
+++ lib/hstore-attributes/hstore_columns.rb
@@ -17,14 +17,12 @@ module HstoreColumns
   end
 
 
+
   module ClassMethods
 
     def hstore_accessor(hstore_column, attr_name, type = :string)
       return unless table_exists?
-      column                      = ActiveRecord::ConnectionAdapters::Column.new(attr_name, nil, "#{type}")
-      columns_hash[attr_name.to_s]= column
-      cast_code                   = column.type_cast_code('v')
-      access_code                 = "(v=read_hstore('#{attr_name}', '#{hstore_column}')) && #{cast_code}"
+      access_code                 = "(v=read_hstore('#{attr_name}', '#{hstore_column}')) && #{type_cast_code('v', type)}"
 
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
             def #{attr_name}; #{access_code}; end
@@ -36,6 +34,23 @@ module HstoreColumns
       accessors = options[:accessors]
       accessors.each do |key, value|
         hstore_accessor store_attribute, key, value.nil? ? :string : value
+      end
+    end
+
+    def type_cast_code(var_name, type)
+      klass = ActiveRecord::ConnectionAdapters::Column
+
+      case type.to_sym
+      when :string, :text        then var_name
+      when :integer              then "(#{var_name}.to_i rescue #{var_name} ? 1 : 0)"
+      when :float                then "#{var_name}.to_f"
+      when :decimal              then "#{klass}.value_to_decimal(#{var_name})"
+      when :datetime, :timestamp then "#{klass}.string_to_time(#{var_name})"
+      when :time                 then "#{klass}.string_to_dummy_time(#{var_name})"
+      when :date                 then "#{klass}.string_to_date(#{var_name})"
+      when :binary               then "#{klass}.binary_to_string(#{var_name})"
+      when :boolean              then "#{klass}.value_to_boolean(#{var_name})"
+      else var_name
       end
     end
 
