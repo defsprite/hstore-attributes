75e0140189b88fec7dccbfa49d9aa721482e7b25
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2013-05-20 11:55:22 +0100

    moving to old hash syntax to test more rubies

--- test/hstore_model.rb
+++ test/hstore_model.rb
@@ -1,8 +1,8 @@
 class HstoreModel < ActiveRecord::Base
   serialize :data, ActiveRecord::Coders::Hstore
 
-  hstore :data, accessors: {:color => :string, :homepage => :string, :available_on => :date}
-  hstore :data, accessors: [:other_color, :other_homepage]
+  hstore :data, :accessors => {:color => :string, :homepage => :string, :available_on => :date}
+  hstore :data, :accessors => [:other_color, :other_homepage]
 
-  hstore :data, accessors: {:string_test => :string, :date_test => :date, :datetime_test => :datetime, :integer_test => :integer}
+  hstore :data, :accessors => {:string_test => :string, :date_test => :date, :datetime_test => :datetime, :integer_test => :integer}
 end
